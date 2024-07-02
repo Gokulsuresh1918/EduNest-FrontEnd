3970b3fe959ee1128d9491b6dd956c91c55aef56
Gokul Suresh s.v <gokulofficial18062@gmail.com>
2024-07-02 10:51:37 +0530

    commemt error solved

--- src/components/classRoom/created/SideBarRight.tsx
+++ src/components/classRoom/created/SideBarRight.tsx
@@ -143,7 +143,7 @@ export default function SideBarRight() {
           </SheetDescription>
         </SheetHeader>
         <div className="grid gap-1 py-4 ">
-          //todo here is an issue
+         
           {students &&
             students.map((student, index) => (
               <div
